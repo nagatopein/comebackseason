9b0a5f33a093348ee8f4b214bedb297a57b3cab6
Taj <betetat94@gmail.com>
2022-06-05 16:36:03 -0400

    updated looks added save feature

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
-// var tasks = [];
+var tasks = [];
 
+// save tasks to local storage
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+// edit tasks
+$("#list1").on("click", function () {
+    var text = $(this).text().trim();
+    console.log(text);
+
+    var textInput = $("<textarea>").addClass("h-100").val(text);
+
+    $(this).replaceWith(textInput);
+
+    textInput.trigger("focus");
+});
+
+$("#save1").on("click", function () {
+    saveTasks();
+    console.log("task saved");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create tasks
 // var createTask = function (taskText, taskList) {
 //     var taskLi = $("<li>").addClass("list-item");
 //     var taskP = $("p").addClass("m-1").text(taskText);
@@ -7,16 +44,54 @@
 //     taskLi.append(taskP);
 
 //     $("#list1" + taskList).append(taskLi);
+
 // };
 
-// // edit tasks feature
-// $("#list1").on("click", function () {
-//     var text = $(this).text().trim();
-//     console.log(text);
+// var loadTasks = function () {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
 
-//     var textInput = $("<textarea>").addClass("h-100").val(text);
+//     // if nothing in localStorage, create a new object to track all task status arrays
+//     // if (!tasks) {
+//     //   tasks = {
+//     //     toDo: [],
+//     //     inProgress: [],
+//     //     inReview: [],
+//     //     done: []
+//     //   };
+//     // }
 
-//     $(this).replaceWith(textInput);
+//     // loop over object properties
+//     $.each(tasks, function (list, arr) {
+//         console.log(list, arr);
+//         // then loop over sub-array
+//         arr.forEach(function (task) {
+//             createTask(task.text, task.date, list);
+//         });
+//     });
+// };
 
-//     textInput.trigger("focus");
-// });
+
+
+
+
+// var colorCode = function () {
+//     $(".time-block").each(function () {
+//         var currentTime = moment().hours();
+//         var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+//         if (timeBlock < currentTime) {
+//             $(this).addClass("past");
+//             $(this).removeClass("present");
+//             $(this).removeClass("future");
+//         }
+//         else if (hourBlock === currentHour) {
+//             $(this).addClass("present");
+//             $(this).removeClass("past");
+//             $(this).removeClass("future");
+//         }
+//         else {
+//             $(this).addClass("future");
+//             $(this).removeClass("past");
+//             $(this).removeClass("present");
+//         }
+//     });
+// }
